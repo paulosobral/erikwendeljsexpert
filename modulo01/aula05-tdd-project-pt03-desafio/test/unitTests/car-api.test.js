89a795945fe097851d9c895da230e1a2933f042c
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-04-22 15:19:49 -0300

    test: finish rent car POST unit test

--- modulo01/aula05-tdd-project-pt03-desafio/test/unitTests/car-api.test.js
+++ modulo01/aula05-tdd-project-pt03-desafio/test/unitTests/car-api.test.js
@@ -1,42 +1,76 @@
 const { describe, it, after, before } = require('mocha')
-
+const sinon = require('sinon')
 const supertest = require('supertest')
 const assert = require('assert')
 
+const mocks = {
+    validCarCategory: require('./../mocks/valid-carCategory.json'),
+    validCar: require('./../mocks/valid-car.json'),
+    validCustomer: require('./../mocks/valid-customer.json')
+}
+
 describe('API Suite test', () => {
     let app
+    let sandbox
     before((done) => {
         app = require('../../src/app')
         app.once('listening', done)
     })
     after(done => app.close(done))
 
+    beforeEach(() => {
+        sandbox = sinon.createSandbox()
+    })
+
+    afterEach(() => {
+        sandbox.restore()
+    })
+
     describe('/rent:post', () => {
         it('should request rent a car and return HTTP Status 200', async () => {
-            const request = {
-                numberOfDays: 10,
-                customer: {
-                    id: "39352a78-8a20-4249-94c8-15dc5f6a7fd6",
-                    name: "Eva Pacocha",
-                    age: 34
-                },
-                carCategory: {
-                    id: "2e93ad8a-6e31-48eb-a565-aeb82c0cc600",
-                    name: "Coupe",
-                    carIds: [
-                    "4125bd3b-cc21-4ace-9839-6c72cd86405c",
-                    "10b3127d-8da7-4c21-a873-019cd9c4a32c",
-                    "d2d392a3-0f93-4920-8d57-441c019b8ca3"
-                ],
-                price: 34.58
-                }
+
+            const car = mocks.validCar
+            const carCategory = {
+                ...mocks.validCarCategory,
+                price: 37.6,
+                carIds: [car.id]
             }
+
+            const customer = Object.create(mocks.validCustomer)
+            customer.age = 20
+
+            const numberOfDays = 5
+            
+
+            const now = new Date(2020, 10, 5)
+            sandbox.useFakeTimers(now.getTime())
+
+
+            const request = {
+                numberOfDays: numberOfDays,
+                customer: customer,
+                carCategory: carCategory
+            }
+
+            const expectedDueDate = "10 de novembro de 2020"
+            const expectedAmount = new Intl.NumberFormat('pt-br', {
+                style: 'currency',
+                currency: 'BRL'
+            }).format(206.8)
+            const expectedResponse = {
+                customer: customer,
+                car: car,
+                amount: expectedAmount,
+                dueDate: expectedDueDate
+            }
+            
             const response = await supertest(app)
             .post('/rent')
             .send(request)
             .expect(200)
 
             assert.ok(response.ok)
+            assert.strictEqual(response.text, JSON.stringify(expectedResponse))
         })
         it('should request rent a car and return HTTP Status 400', async () => {
             const response = await supertest(app)
