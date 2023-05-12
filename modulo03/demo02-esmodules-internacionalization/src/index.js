5ebfc96006aab78bcfa8d61b9631cf480962da1e
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-05-11 21:56:11 -0300

    feat: class demo02-esmodules-internacionalization-pt2

--- modulo03/demo02-esmodules-internacionalization/src/index.js
+++ modulo03/demo02-esmodules-internacionalization/src/index.js
@@ -1,32 +1,29 @@
-import DraftLog from 'draftlog'
-import chalk from 'chalk'
-import chalkTable from 'chalk-table'
-import readLine from 'readline'
-
 import database from './../database.json'
 import Person from './person.js'
+import TerminalController from './terminalController.js'
 
-DraftLog(console).addLineListener(process.stdin)
 const DEFAULT_LANG = "pt-BR"
-const options = {
-    leftPad:2,
-    columns: [
-        {field: "id", name: chalk.cyan("ID")},
-        {field: "vehicles", name: chalk.magenta("Vehicles")},
-        {field: "kmTraveled", name: chalk.cyan("KM Traveled")},
-        {field: "from", name: chalk.cyan("From")},
-        {field: "to", name: chalk.cyan("To")},
-    ]
+const STOP_TERM = ":q"
+
+const terminalController = new TerminalController()
+terminalController.initializeTerminal(database, DEFAULT_LANG)
+
+async function mainLoop() {
+    try {
+        const answer = await terminalController.question()
+        if(answer === STOP_TERM) {
+            terminalController.closeTerminal()
+            console.log('process finished!')
+            return;
+        }
+        const person = Person.generateInstanceFromString(answer)
+        
+        return mainLoop()
+
+    } catch (error) {
+        console.error('DEU RUIM**', error)
+        return mainLoop()
+    }
 }
 
-const table = chalkTable(options, database.map(item => new Person(item).formatted(DEFAULT_LANG)))
-const print = console.draft(table)
-
-const terminal = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-terminal.question('Qual é o seu nome?', msg => {
-    console.log('msg', msg.toString())
-})
+await mainLoop()
