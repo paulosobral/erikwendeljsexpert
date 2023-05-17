a1602ada9ed047a937081fabde4cfa2f216324f0
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-05-16 22:34:38 -0300

    feat: class demo02-esmodules-internacionalization-pt3

--- modulo03/demo02-esmodules-internacionalization/src/index.js
+++ modulo03/demo02-esmodules-internacionalization/src/index.js
@@ -1,8 +1,12 @@
 import database from './../database.json'
 import Person from './person.js'
 import TerminalController from './terminalController.js'
+import { save } from './repository.js'
 
 const DEFAULT_LANG = "pt-BR"
+// const DEFAULT_LANG = "es"
+// const DEFAULT_LANG = "en"
+// const DEFAULT_LANG = "rus"
 const STOP_TERM = ":q"
 
 const terminalController = new TerminalController()
@@ -17,7 +21,9 @@ async function mainLoop() {
             return;
         }
         const person = Person.generateInstanceFromString(answer)
-        
+        terminalController.updateTable(person.formatted(DEFAULT_LANG))
+        await save(person)
+
         return mainLoop()
 
     } catch (error) {
--- modulo03/demo02-esmodules-internacionalization/src/terminalController.js
+++ modulo03/demo02-esmodules-internacionalization/src/terminalController.js
@@ -27,6 +27,10 @@ export default class TerminalController {
         this.print = console.draft(table)
         this.data = data
     }
+    updateTable(item) {
+        this.data.push(item)
+        this.print(chalkTable(this.getTableOptions(), this.data))
+    }
     question(msg = '') {
         return new Promise(resolve => this.terminal.question(msg, resolve))
     }
