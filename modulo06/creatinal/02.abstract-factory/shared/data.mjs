583669d2eb25cbbe4be36606bcbe699afe51f9f1
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-08-05 11:16:15 -0300

    feat: class 02 module 06

--- modulo06/creatinal/02.abstract-factory/shared/data.mjs
+++ modulo06/creatinal/02.abstract-factory/shared/data.mjs
@@ -1,268 +1,65 @@
 export const database = [
-    [
-        {
-          "index": 0,
-          "guid": "a5ac9df0-931b-456f-b018-6c125b46d351",
-          "isActive": true,
-          "balance": "$3,946.33",
-          "picture": "http://placehold.it/32x32",
-          "age": 34,
-          "eyeColor": "blue",
-          "name": "Allen Patterson",
-          "gender": "male",
-          "company": "SHADEASE",
-          "email": "allenpatterson@shadease.com",
-          "phone": "+1 (803) 440-2525",
-          "address": "181 Horace Court, Ronco, California, 2614",
-          "about": "Lorem laborum proident anim fugiat dolore veniam do. Aliquip commodo adipisicing magna magna non ullamco culpa eu sit incididunt nisi. Pariatur tempor culpa ea id et veniam ad est enim id ullamco quis.\r\n",
-          "registered": "2020-03-17T01:10:51 +03:00",
-          "latitude": 42.721341,
-          "longitude": 53.270462,
-          "tags": [
-            "sit",
-            "excepteur",
-            "voluptate",
-            "velit",
-            "eu",
-            "labore",
-            "laboris"
-          ],
-          "friends": [
-            {
-              "id": 0,
-              "name": "Koch Richard"
-            },
-            {
-              "id": 1,
-              "name": "Sue Livingston"
-            },
-            {
-              "id": 2,
-              "name": "Mcfarland Dennis"
-            }
-          ],
-          "greeting": "Hello, Allen Patterson! You have 10 unread messages.",
-          "favoriteFruit": "apple"
-        },
-        {
-          "index": 1,
-          "guid": "ac4b6e0d-2f7a-4c08-86a8-b25f0a005bb5",
-          "isActive": true,
-          "balance": "$3,549.31",
-          "picture": "http://placehold.it/32x32",
-          "age": 40,
-          "eyeColor": "blue",
-          "name": "Ines Santana",
-          "gender": "female",
-          "company": "STREZZO",
-          "email": "inessantana@strezzo.com",
-          "phone": "+1 (818) 470-3119",
-          "address": "653 Eldert Lane, Gulf, South Carolina, 7841",
-          "about": "Labore magna cillum exercitation quis sint fugiat aute eu in excepteur esse. Nisi fugiat Lorem irure Lorem tempor labore aliquip ullamco. Fugiat velit esse dolor minim est elit consectetur ipsum nulla. Velit labore et tempor pariatur ad mollit qui dolor sint est reprehenderit Lorem proident.\r\n",
-          "registered": "2022-11-24T02:03:27 +03:00",
-          "latitude": -27.265096,
-          "longitude": 172.211823,
-          "tags": [
-            "officia",
-            "exercitation",
-            "velit",
-            "magna",
-            "nostrud",
-            "cupidatat",
-            "magna"
-          ],
-          "friends": [
-            {
-              "id": 0,
-              "name": "Trisha Salas"
-            },
-            {
-              "id": 1,
-              "name": "Le Merrill"
-            },
-            {
-              "id": 2,
-              "name": "Edith Diaz"
-            }
-          ],
-          "greeting": "Hello, Ines Santana! You have 3 unread messages.",
-          "favoriteFruit": "banana"
-        },
-        {
-          "index": 2,
-          "guid": "8704dfb3-175a-4656-90d3-4ad2ded3be2f",
-          "isActive": true,
-          "balance": "$2,172.68",
-          "picture": "http://placehold.it/32x32",
-          "age": 24,
-          "eyeColor": "brown",
-          "name": "Bowen Cole",
-          "gender": "male",
-          "company": "CODAX",
-          "email": "bowencole@codax.com",
-          "phone": "+1 (991) 424-3451",
-          "address": "589 Lefferts Place, Emison, Utah, 7475",
-          "about": "Mollit exercitation ut eiusmod in duis anim proident deserunt pariatur elit. Reprehenderit mollit esse enim incididunt magna dolor irure nostrud minim aute amet nisi ullamco. Ullamco deserunt elit aliqua commodo. Dolore ad nulla enim pariatur labore labore.\r\n",
-          "registered": "2014-05-21T07:07:05 +03:00",
-          "latitude": 50.120731,
-          "longitude": -79.888307,
-          "tags": [
-            "officia",
-            "do",
-            "sit",
-            "do",
-            "minim",
-            "est",
-            "velit"
-          ],
-          "friends": [
-            {
-              "id": 0,
-              "name": "Mayra Jensen"
-            },
-            {
-              "id": 1,
-              "name": "Kline Baldwin"
-            },
-            {
-              "id": 2,
-              "name": "Geraldine Smith"
-            }
-          ],
-          "greeting": "Hello, Bowen Cole! You have 2 unread messages.",
-          "favoriteFruit": "banana"
-        },
-        {
-          "index": 3,
-          "guid": "7af416f2-5e01-4af7-a6d1-b0442ab321aa",
-          "isActive": true,
-          "balance": "$1,146.47",
-          "picture": "http://placehold.it/32x32",
-          "age": 35,
-          "eyeColor": "brown",
-          "name": "Travis Leach",
-          "gender": "male",
-          "company": "FRANSCENE",
-          "email": "travisleach@franscene.com",
-          "phone": "+1 (876) 511-2480",
-          "address": "744 Strauss Street, Gerton, Hawaii, 4790",
-          "about": "Irure tempor culpa excepteur excepteur tempor est id ex aute amet occaecat qui. Occaecat tempor culpa proident aliquip consectetur voluptate esse laborum nisi sit ipsum elit. Deserunt minim officia amet voluptate quis sint voluptate incididunt exercitation. Sint fugiat consectetur occaecat veniam elit velit. Nostrud dolor quis reprehenderit magna veniam ad eiusmod veniam. Magna tempor sunt amet consectetur id consectetur ut pariatur consequat veniam aliqua adipisicing Lorem minim.\r\n",
-          "registered": "2016-02-04T01:03:55 +02:00",
-          "latitude": -48.208982,
-          "longitude": -71.933016,
-          "tags": [
-            "culpa",
-            "commodo",
-            "adipisicing",
-            "aliqua",
-            "sint",
-            "deserunt",
-            "nostrud"
-          ],
-          "friends": [
-            {
-              "id": 0,
-              "name": "Blanche Thompson"
-            },
-            {
-              "id": 1,
-              "name": "Agnes Cobb"
-            },
-            {
-              "id": 2,
-              "name": "Wolf Mullins"
-            }
-          ],
-          "greeting": "Hello, Travis Leach! You have 6 unread messages.",
-          "favoriteFruit": "apple"
-        },
-        {
-          "index": 4,
-          "guid": "035e151a-419b-411d-be4b-630e30b25725",
-          "isActive": true,
-          "balance": "$3,258.92",
-          "picture": "http://placehold.it/32x32",
-          "age": 32,
-          "eyeColor": "green",
-          "name": "Duffy Duffy",
-          "gender": "male",
-          "company": "UNQ",
-          "email": "duffyduffy@unq.com",
-          "phone": "+1 (848) 474-2876",
-          "address": "258 Melba Court, Edinburg, Kentucky, 5126",
-          "about": "Reprehenderit adipisicing nisi incididunt ipsum duis sit occaecat deserunt ea. Sunt labore non dolore mollit incididunt aliquip voluptate elit est mollit. Do id irure nisi quis. Consectetur dolor est tempor veniam eiusmod eu sunt dolor eiusmod. Mollit amet magna qui irure quis minim.\r\n",
-          "registered": "2020-07-04T09:50:51 +03:00",
-          "latitude": -10.359197,
-          "longitude": -1.468865,
-          "tags": [
-            "consectetur",
-            "minim",
-            "nisi",
-            "qui",
-            "est",
-            "excepteur",
-            "ea"
-          ],
-          "friends": [
-            {
-              "id": 0,
-              "name": "Ada Sweet"
-            },
-            {
-              "id": 1,
-              "name": "Pearson Hansen"
-            },
-            {
-              "id": 2,
-              "name": "Boyer Holman"
-            }
-          ],
-          "greeting": "Hello, Duffy Duffy! You have 1 unread messages.",
-          "favoriteFruit": "apple"
-        },
-        {
-          "index": 5,
-          "guid": "c9af5fc2-0b8e-46fa-b65b-16e803ff6e64",
-          "isActive": true,
-          "balance": "$2,212.80",
-          "picture": "http://placehold.it/32x32",
-          "age": 23,
-          "eyeColor": "green",
-          "name": "Ellen Lawson",
-          "gender": "female",
-          "company": "KOFFEE",
-          "email": "ellenlawson@koffee.com",
-          "phone": "+1 (957) 484-2013",
-          "address": "468 Glendale Court, Edmund, Texas, 2518",
-          "about": "Sint esse irure mollit culpa dolor reprehenderit ex non cupidatat fugiat ea aliqua. Sunt cillum ex incididunt esse voluptate dolore pariatur qui eu elit do proident labore consequat. Exercitation minim dolor quis ex id id anim ut.\r\n",
-          "registered": "2023-02-25T04:41:50 +03:00",
-          "latitude": 36.286032,
-          "longitude": 9.681734,
-          "tags": [
-            "non",
-            "commodo",
-            "ea",
-            "anim",
-            "Lorem",
-            "voluptate",
-            "aliqua"
-          ],
-          "friends": [
-            {
-              "id": 0,
-              "name": "Coleman Casey"
-            },
-            {
-              "id": 1,
-              "name": "Lupe Phelps"
-            },
-            {
-              "id": 2,
-              "name": "Pollard Ramsey"
-            }
-          ],
-          "greeting": "Hello, Ellen Lawson! You have 4 unread messages.",
-          "favoriteFruit": "banana"
-        }
-      ]
+    {
+      "balance": "$2,642.58",
+      "age": 28,
+      "name": "Sarah Dickerson",
+      "company": "BICOL",
+      "email": "sarah dickerson@bicol.org",
+      "phone": "+1 (974) 464-2114",
+      "address": "149 Micieli Place, Kimmell, Massachusetts, 8060"
+    },
+    {
+      "balance": "$3,949.91",
+      "age": 25,
+      "name": "Haynes Fry",
+      "company": "ECRAZE",
+      "email": "haynes fry@ecraze.biz",
+      "phone": "+1 (981) 460-2730",
+      "address": "478 Monument Walk, Kohatk, Connecticut, 2562"
+    },
+    {
+      "balance": "$1,540.29",
+      "age": 24,
+      "name": "Marissa Brewer",
+      "company": "KAGGLE",
+      "email": "marissa brewer@kaggle.io",
+      "phone": "+1 (885) 442-3730",
+      "address": "513 Auburn Place, Reinerton, Marshall Islands, 6163"
+    },
+    {
+      "balance": "$1,121.57",
+      "age": 26,
+      "name": "Melinda Guy",
+      "company": "HOPELI",
+      "email": "melinda guy@hopeli.co.uk",
+      "phone": "+1 (976) 434-2594",
+      "address": "835 Ocean Court, Veguita, Kentucky, 4200"
+    },
+    {
+      "balance": "$1,610.08",
+      "age": 28,
+      "name": "Duncan Santiago",
+      "company": "BOSTONIC",
+      "email": "duncan santiago@bostonic.me",
+      "phone": "+1 (814) 570-3942",
+      "address": "386 Rutherford Place, Elliston, North Carolina, 5784"
+    },
+    {
+      "balance": "$1,273.35",
+      "age": 23,
+      "name": "Perkins Albert",
+      "company": "RUGSTARS",
+      "email": "perkins albert@rugstars.net",
+      "phone": "+1 (942) 594-2033",
+      "address": "994 Ryerson Street, Dotsero, Arizona, 3922"
+    },
+    {
+      "balance": "$3,210.89",
+      "age": 39,
+      "name": "Compton Frazier",
+      "company": "TERASCAPE",
+      "email": "compton frazier@terascape.tv",
+      "phone": "+1 (889) 579-2174",
+      "address": "297 Howard Alley, Alfarata, Ohio, 1842"
+    }
 ]
