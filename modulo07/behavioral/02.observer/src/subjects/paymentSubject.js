98ae9d86c4d273676d7c4a7a0f58e2565585e1ad
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-08-21 21:27:21 -0300

    wip: class 02 module 7

--- modulo07/behavioral/02.observer/src/subjects/paymentSubject.js
+++ modulo07/behavioral/02.observer/src/subjects/paymentSubject.js
@@ -1,12 +1,13 @@
 export default class PaymentSubject {
+    #observers = new Set()
     notify(data) {
-
+        this.#observers.forEach(observer => observer.update(data));
     }
     unsubscribe(observable) {
-        
+        this.#observers.delete(observable)
     }
 
     subscribe(observable) {
-
+        this.#observers.add(observable)
     }
 }
--- modulo07/behavioral/02.observer/test/index.test.js
+++ modulo07/behavioral/02.observer/test/index.test.js
@@ -1,6 +1,50 @@
 import { expect, describe, test, jest} from '@jest/globals'
+import PaymentSubject from '../src/subjects/paymentSubject.js'
+import Payment from '../src/events/payment.js'
 
 describe('Test Suite for Observer Pattern', () => {
 
+    test('#PaymentSubject notify observers', () => {
+        const subject = new PaymentSubject()
+        const observer = {
+            update: jest.fn()
+        }
+        const data = 'hello world'
+        const expected = data
+
+        subject.subscribe(observer)
+        subject.notify(data)
+
+        expect(observer.update).toBeCalledWith(expected)
+    })
+    test('#PaymentSubject should not notify unsubscribed observers', () => {
+        const subject = new PaymentSubject()
+        const observer = {
+            update: jest.fn()
+        }
+        const data = 'hello world'
+        
+
+        subject.subscribe(observer)
+        subject.unsubscribe(observer)
+        subject.notify(data)
+
+        expect(observer.update).not.toHaveBeenCalled()
+    })
+
+    test('#Payment should notify subject after a credit card transaction', () => {
+        const subject = new PaymentSubject()
+        const payment = new Payment(subject)
+
+        const paymentSubjectNotifierSpy = jest.spyOn(
+            payment.paymentSubject,
+            payment.paymentSubject.notify.name,
+        )
+        const data = { userName: 'erickwendel', id: Date.now() }
+        payment.creditCard(data)
+
+        expect(paymentSubjectNotifierSpy).toBeCalledWith(data)
+    })
+    test.todo('#All should notify subscribers after a credit card payment')
 })
-// -21:03 https://training.erickwendel.com.br/92103-javascript-expert/2196678-o-padrao-observer-jest-e-100-de-cobertura-de-codigo
+// -11:03 https://training.erickwendel.com.br/92103-javascript-expert/2196678-o-padrao-observer-jest-e-100-de-cobertura-de-codigo
