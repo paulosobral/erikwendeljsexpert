c0e1e0f863f7d5b01b36cf2c15fa1b4a3f99c823
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-09-04 19:42:47 -0300

    feat: class 03 module 07

--- modulo07/behavioral/02.observer/test/index.test.js
+++ modulo07/behavioral/02.observer/test/index.test.js
@@ -1,9 +1,13 @@
-import { expect, describe, test, jest} from '@jest/globals'
+import { expect, describe, test, jest, beforeAll} from '@jest/globals'
 import PaymentSubject from '../src/subjects/paymentSubject.js'
 import Payment from '../src/events/payment.js'
+import Shipment from '../src/observers/shipment.js'
+import Marketing from '../src/observers/marketing.js'
 
 describe('Test Suite for Observer Pattern', () => {
-
+    beforeAll(() => {
+        jest.spyOn(console, console.log.name).mockImplementation(() => {})
+    })
     test('#PaymentSubject notify observers', () => {
         const subject = new PaymentSubject()
         const observer = {
@@ -45,6 +49,23 @@ describe('Test Suite for Observer Pattern', () => {
 
         expect(paymentSubjectNotifierSpy).toBeCalledWith(data)
     })
-    test.todo('#All should notify subscribers after a credit card payment')
+    test('#All should notify subscribers after a credit card payment', () => {
+        const subject = new PaymentSubject()
+        const shipment = new Shipment()
+        const marketing = new Marketing()
+
+        const shipmentSpy = jest.spyOn(shipment, shipment.update.name)
+        const marketingSpy = jest.spyOn(marketing, marketing.update.name)
+
+        subject.subscribe(shipment)
+        subject.subscribe(marketing)
+
+        const payment = new Payment(subject)
+        const data = { userName: 'erickwebdel', id: Date.now() }
+        payment.creditCard(data)
+
+        expect(shipmentSpy).toBeCalledWith(data)
+        expect(marketingSpy).toBeCalledWith(data)
+
+    })
 })
-// -11:03 https://training.erickwendel.com.br/92103-javascript-expert/2196678-o-padrao-observer-jest-e-100-de-cobertura-de-codigo
