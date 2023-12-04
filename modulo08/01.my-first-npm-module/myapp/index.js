6da1923c5f1cddbbe91b02a8fd031ce61ca27c35
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-12-04 20:06:14 -0300

    wip: class 02 module 8

--- modulo08/01.my-first-npm-module/myapp/index.js
+++ modulo08/01.my-first-npm-module/myapp/index.js
@@ -1,9 +1,9 @@
 // para importar do diretório use o comando abaixo
-// --experimental-specifier-resolution=node
+// node --experimental-specifier-resolution=node
 // import FluentSQLBuilder from "./../fluentsql-jest-tdd-yt"
-import FluentSQLBuilder from "./../fluentsql-jest-tdd-yt"
+import FluentSQLBuilder from "@psobral89/fluentsql"
 
-import database from "./database/data.json"
+import database from "./database/data.json" assert { type: 'json' };
 
 const result = FluentSQLBuilder.for(database)
     .where({ registered: /^(2020|2019)/ })
@@ -12,4 +12,4 @@ const result = FluentSQLBuilder.for(database)
     .build()
 
 console.log(result)
-//TODO: -20:17 https://training.erickwendel.com.br/92103-javascript-expert/2196684-criando-e-gerenciando-meu-primeiro-pacote-npm
+//TODO: -14:37 https://training.erickwendel.com.br/92103-javascript-expert/2196684-criando-e-gerenciando-meu-primeiro-pacote-npm
