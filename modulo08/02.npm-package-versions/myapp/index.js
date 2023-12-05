83b036952413c371567f5cae89e523aa1c2c0188
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-12-05 19:09:41 -0300

    wip: class 03 module 08

---
+++ modulo08/02.npm-package-versions/myapp/index.js
@@ -0,0 +1,17 @@
+// para importar do diretório use o comando abaixo
+// node --experimental-specifier-resolution=node
+// import FluentSQLBuilder from "./../fluentsql-jest-tdd-yt"
+import FluentSQLBuilder from "@psobral89/fluentsql"
+
+import database from "./database/data.json" assert { type: 'json' };
+
+const result = FluentSQLBuilder.for(database)
+    .where({ registered: /^(2020|2019)/ })
+    .select(['name'])
+    .limit(3)
+    .countBy('name')
+    //.groupCount('name')
+    .build()
+
+console.log(result)
+//TODO: -14:37 https://training.erickwendel.com.br/92103-javascript-expert/2196684-criando-e-gerenciando-meu-primeiro-pacote-npm
