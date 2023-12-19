eb369c3c2a791a21ddc7805694e9dd8c5ecd9e79
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-12-19 07:41:26 -0300

    feat: class 03 module 08

--- modulo08/03.npm-registry/fluentsql/index.js
+++ modulo08/03.npm-registry/fluentsql/index.js
@@ -1,5 +1,3 @@
 import FluentSQLBuilder from "./src/fluentSQL.js";
 
 export default FluentSQLBuilder
-
-//TODO: -14:22 https://training.erickwendel.com.br/92103-javascript-expert/2196724-distribuindo-pacotes-na-web-usando-npm-github-e-verdaccio-multiplos-perf
