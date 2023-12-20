b9412869de2a5a3ecc7e4ec45cf70c5a2f4d44d0
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-12-20 19:30:38 -0300

    feat: class 05 module 08

--- modulo08/04.npm-workspaces/workspaces/date-util/index.test.js
+++ modulo08/04.npm-workspaces/workspaces/date-util/index.test.js
@@ -8,3 +8,105 @@ import DateUtil from './index.js'
     const result = DateUtil.formatDate(date, format)
     deepStrictEqual(result, expected)
 }
+
+{
+    const expected = '01-12-1990'
+    const format = 'dd-mm-yyyy'
+    const date = new Date('1990-12-01')
+    const result = DateUtil.formatDate(date, format)
+    deepStrictEqual(result, expected)
+}
+
+{
+    const expected = '22/06/1995'
+    const format = 'dd/mm/yyyy'
+    const date = new Date('1995-06-22')
+    const result = DateUtil.formatDate(date, format)
+    deepStrictEqual(result, expected)
+}
+
+{
+    const expected = '1995-06-22'
+    const format = 'yyyy-mm-dd'
+    const date = new Date('1995-06-22')
+    const result = DateUtil.formatDate(date, format)
+    deepStrictEqual(result, expected)
+}
+
+/// formatString
+
+{
+    const expected = { error: 'your text is empty'}
+    const date = ''
+    const result = DateUtil.formatString(date)
+    deepStrictEqual(result, expected)
+}
+
+{
+    
+    const date = {
+        value: '1990-april-01',
+        format: 'yyyy-M-dd',
+    }
+
+    const expected = { error: `the format ${date.format} is not available yet :(` }
+
+    const result = DateUtil.formatString(date.value, date.format)
+    deepStrictEqual(result, expected)
+}
+
+{
+    
+    const date = {
+        value: '1990-01-01',
+        format: 'yyyy-mm-dd',
+    }
+    const expectedFormat = 'dd/M/yyyy'
+
+    const expected = { error: `the format ${expectedFormat} is not available yet :(` }
+
+    const result = DateUtil.formatString(date.value, date.format, expectedFormat)
+    deepStrictEqual(result, expected)
+}
+
+{
+    
+    const date = {
+        value: '1990-01-01',
+        format: 'yyyy-mm-dd',
+    }
+    const expectedFormat = 'dd-mm-yyyy'
+
+    const expected = '01-01-1990'
+
+    const result = DateUtil.formatString(date.value, date.format, expectedFormat)
+    deepStrictEqual(result, expected)
+}
+
+{
+    
+    const date = {
+        value: ' 1 9 9 0 / 0 7 / 0 1 ',
+        format: 'yyyy/mm/dd',
+    }
+    const expectedFormat = 'yyyy-mm-dd'
+
+    const expected = '1990-07-01'
+
+    const result = DateUtil.formatString(date.value, date.format, expectedFormat)
+    deepStrictEqual(result, expected)
+}
+
+{
+    
+    const date = {
+        value: ' 0 1 / 08/ 1 9 9 0 ',
+        format: 'dd/mm/yyyy',
+    }
+    const expectedFormat = 'yyyy-mm-dd'
+
+    const expected = '1990-08-01'
+
+    const result = DateUtil.formatString(date.value, date.format, expectedFormat)
+    deepStrictEqual(result, expected)
+}
