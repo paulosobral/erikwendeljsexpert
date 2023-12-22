f83722e0890c5f9dc632e9cc78c0224179435ff4
Paulo Sobral <paulo.sobral@outlook.com.br>
2023-12-22 07:29:50 -0300

    feat: class 03 module 09

---
+++ modulo09/02.bash-vs-js/exemplo02/index.mjs
@@ -0,0 +1,27 @@
+import { setTimeout } from 'timers/promises'
+import { $ } from "zx/core"
+import isSafe from 'safe-regex'
+
+
+$.verbose = false
+await $`docker run -p "8080:80" -d nginx`
+await setTimeout(500)
+const req = await $`curl -s localhost:8080`
+console.log(`req\n`, req.stdout)
+
+const containers = await $`docker ps`
+
+// unsafe!
+// const exp = /(?<containerId>\w+)\W+(?=nginx)(x+x+)+y/
+const exp = /(?<containerId>\w+)\W+(?=nginx)/
+if(!isSafe(exp)) 
+    throw new Error('unsafe regex!!')
+
+const { groups: { containerId } } = containers.toString().match(exp)
+console.log(containerId)
+
+const logs = await $`docker logs ${containerId}`
+console.log('logs\n', logs.stdout)
+
+const rm = await $`docker rm -f ${containerId}`
+console.log('rm -f\n', rm.stdout)
