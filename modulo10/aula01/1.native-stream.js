98929c357ec0b170a9a6cf3d68932e79bf69a0fa
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-01-22 06:41:07 -0300

    wip: class 02 module 10

--- modulo10/aula01/1.native-stream.js
+++ modulo10/aula01/1.native-stream.js
@@ -1,4 +1,16 @@
-process.stdin.pipe(process.stdout)
-.on('data', msg => console.log('data', msg))
+// ls | grep package | xargs cat | jq .name
 
-// -50:19 https://training.erickwendel.com.br/92103-javascript-expert/2196736-read-write-duplex-e-transform-streams-o-que-sao-e-categorias
+// process.stdin.pipe(process.stdout)
+
+//     .on('data', msg => console.log('data', msg.toString()))
+//     .on('error', msg => console.log('error', msg.toString()))
+//     .on('end', _ => console.log('end', msg.toString()))
+//     .on('close', _ => console.log('close', msg.toString()))
+
+// terminal 1
+// node -e "require('net').createServer(socket => socket.pipe(process.stdout)).listen(1338)"
+
+// terminal 2
+// node -e "process.stdin.pipe(require('net').connect(1338))"
+
+// -43:15 https://training.erickwendel.com.br/92103-javascript-expert/2196736-read-write-duplex-e-transform-streams-o-que-sao-e-categorias
