9fe733df7f2a82964dc33219a370029b4bdf65fd
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-02-20 08:25:58 -0300

    feat: class 02 module 10

--- modulo10/aula02/examples/1.concat-streams.pass-throught.js
+++ modulo10/aula02/examples/1.concat-streams.pass-throught.js
@@ -1,4 +1,4 @@
-import { Writable } from 'stream'
+import { Writable, PassThrough } from 'stream'
 
 import axios from 'axios'
 const API_01 = 'http://localhost:3000'
@@ -17,16 +17,37 @@ const requests = await Promise.all([
     })
 ])
 
-const result = requests.map(({ data }) => data)
+const results = requests.map(({ data }) => data)
 
 
 const output = Writable({
     write(chunk, encoding, callback) {
-        const data = chunk.toString()
-        console.log('data', data)
+        const data = chunk.toString().replace(/\n/, "")
+        // ?=- -> ele faz procurar a partir do - e olhar para traz
+        // :"(?<name>.*) -> procura pelo conteúdo dentro das aspas após o : e extrai somente o name
+        const name = data.match(/:"(?<name>.*)(?=-)/).groups.name
+        console.log(`[${name.toLowerCase()}] ${data}`)
         callback()
     }
 })
-result[0].pipe(output)
-result[1].pipe(output)
-// TODO: 22:47 https://training.erickwendel.com.br/92103-javascript-expert/2196737-consumindo-streams-de-apis-em-paralelo-node-js-streams-como-async-iterat
+
+function merge(streams) {
+    return streams.reduce((prev, current, index, items) => {
+        // impede que a stream feche sozinha
+        current.pipe(prev, { end: false })
+
+        // como colocamos end: false, vamos manipular manualmente quando o nosso current terminar
+        // Quando ele terminar, vamos verificar se todos no pipeline se encerraram
+        // ele vai então forçar a cadeia do anterior a se fechar
+        current.on('end', () => items.every(s => s.ended) && prev.end())
+        
+        return prev
+
+    }, new PassThrough())
+}
+
+
+merge(results)
+    .pipe(output)
+//results[0].pipe(output)
+//results[1].pipe(output)
