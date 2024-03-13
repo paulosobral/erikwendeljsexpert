da3d3c47041af4ef8b255c00daa3431f4fd66f74
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-03-13 13:05:28 -0300

    wip: class 01 module11

--- modulo11/aula01-error-handling-nodejs/1-server-error.js
+++ modulo11/aula01-error-handling-nodejs/1-server-error.js
@@ -1,20 +1,30 @@
 import Http from 'http'
 
+let count = 1;
 async function handler(request, response) {
+    count++;
     try {
-        
-        for await(const data of request) {
-        
+        if(count % 2 === 0) {
+            await Promise.reject('error dentro do handler!')
+        }
+
+        for await (const data of request) {
+            if(count % 2 !== 0) {
+                await Promise.reject('error dentro do for!')
+            }
             response.end()
         }
     } 
     
     catch (error) {
-        
+        console.log('a server error has happened', error)
+        response.writeHead(500)
+        response.write(JSON.stringify({ message: 'internal server error!'}))
+        response.end()
     }
 }
 
 Http.createServer(handler)
     .listen(3000, () => console.log('running at 3000'))
 // curl -i -X POST -d '{}' localhost:3000
-// https://training.erickwendel.com.br/92103-javascript-expert/2196741-conhecendo-erros-nao-capturados-pelo-node-js
+// TODO: 08:02 https://training.erickwendel.com.br/92103-javascript-expert/2196741-conhecendo-erros-nao-capturados-pelo-node-js
