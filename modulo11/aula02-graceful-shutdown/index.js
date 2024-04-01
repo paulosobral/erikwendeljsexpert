97bc8e1c70e9b1b13218c1a51bbdbef16e035100
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-04-01 16:14:55 -0300

    wip: class 02 module 11

--- modulo11/aula02-graceful-shutdown/index.js
+++ modulo11/aula02-graceful-shutdown/index.js
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb"
-
+import { createServer } from "http"
 async function dbConnect() {
     const client = new MongoClient("mongodb://user:pass@localhost:27017")
     await client.connect()
@@ -14,11 +14,46 @@ async function dbConnect() {
 const { collections, client } = await dbConnect()
 
 async function handler(request, response) {
-    await collections.heroes.insertOne({ updatedAt: new Date().toUTCString(), name: 'Flash' })
-    const heroes = await collections.heroes.find().toArray()
-    console.log({ heroes })
+    for await(const data of request) {
+        try {
+            const hero = JSON.parse(data)
+            await collections.heroes.insertOne({ 
+                ...hero,
+                updatedAt: new Date().toISOString()
+            })
+            const heroes = await collections.heroes.find().toArray()
+            console.log({ heroes })
+            response.writeHead(200)
+            response.write(JSON.stringify(heroes))
+
+        } catch (error) {
+            console.error('a request error has happened', error)
+            response.writeHead(500)
+            response.write(JSON.stringify({ message: 'Internal server error' }))
+        }
+        finally {
+            response.end()
+        }
+    }
 
 }
 
-await client.close()
+// await client.close()
+/*
+    curl -i localhost:3000 -X POST -d '{"name": "Batman", "age": "80"}'
+*/
+
+const server = createServer(handler)
+.listen(3000, () => console.log('running at 3000 and process', process.pid))
+
+
+const onStop = async (signal) => {
+    console.info(`\n${signal} signal received!`)
+
+    // ero é tudo certo, 1 é erro!
+    process.exit(0);
+}
+// SIGINT -> Ctrl c
+// SIGINT -> KILL
+["SIGINT", "SIGTERM"].forEach((event) => process.on(event, onStop))
 // TODO: 09:28 https://training.erickwendel.com.br/92103-javascript-expert/2196742-graceful-shutdown-tecnicas-para-escalar-aplicacoes-node-js-sem-complicac
