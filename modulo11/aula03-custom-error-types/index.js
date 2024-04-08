17137f177f3b1a38e7b2fc7548af47d3845d4034
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-04-08 11:53:10 -0300

    feat: class 03 module 11

--- modulo11/aula03-custom-error-types/index.js
+++ modulo11/aula03-custom-error-types/index.js
@@ -1,14 +1,41 @@
 import { createServer } from 'http'
+import BusinessError from './errors/businessError.js'
+import { statusCodes } from './util/httpStatusCodes.js'
+
+function validateHero(hero) {
+    // simulando um outro erro, por exemplo de banco de dados
+    if(Reflect.has(hero, "connectionError")) {
+        // só um erro genérico para trazer outro cenário de erro inexperado
+        throw new Error('error connection to DB!')
+    }
+
+    if(hero.age < 20) {
+        throw new BusinessError('age must be higher than 20!')
+    }
+
+    if(hero.name?.lenght < 4) {
+        throw new BusinessError('name must be higher than 4!')
+    }
+
+
+}
+
 async function handler(request, response) {
     for await (const data of request) {
         try {
             const hero = JSON.parse(data)
+            validateHero(hero)
             console.log({ hero })
-            response.writeHead(200)
+            response.writeHead(statusCodes.OK)
             response.end()
 
         } catch (error) {
-            response.writeHead(500)
+            if(error instanceof BusinessError) {
+                response.writeHead(statusCodes.BAD_REQUEST)
+                response.end(error.message)
+                continue
+            }
+            response.writeHead(statusCodes.INTERNAL_SERVER_ERROR)
             response.end()
         }
     }
