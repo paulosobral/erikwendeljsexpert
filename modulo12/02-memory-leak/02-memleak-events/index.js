1c05aafd1a940c19566c1d3745170b277940e6d2
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-07-03 08:35:11 -0300

    feat: class 04 module 12

--- modulo12/02-memory-leak/02-memleak-events/index.js
+++ modulo12/02-memory-leak/02-memleak-events/index.js
@@ -1,14 +1,21 @@
 import { createServer} from 'http'
 
 import Events from 'events'
-
+import { randomBytes } from 'crypto'
 const myEvent = new Events()
-function onData() {
-    const items = []
-    setInterval(() => items.push(Date.now()))
+
+function getBbytes() {
+    return randomBytes(10000)
 }
 
-createServer((_, response) => {
+function onData() {
+    getBbytes()
+    const items = []
+    // setInterval(function myInterval() { items.push(Date.now())})
+}
+
+
+createServer(function handler(_, response) {
     myEvent.on('data', onData)
 
     myEvent.emit('data', Date.now())
