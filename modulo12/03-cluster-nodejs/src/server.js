d86e8fd885a66274af56a122c08fc953ac48dac2
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-07-06 10:15:59 -0300

    wip: class 5 module 12

--- modulo12/03-cluster-nodejs/src/server.js
+++ modulo12/03-cluster-nodejs/src/server.js
@@ -2,11 +2,15 @@ import { createServer } from 'http'
 import { appendFile } from 'fs'
 
 async function handler(request, response) {
-    await appendFile('.log.txt', `processed by ${process.pid}\n`)
-    const result = Array.from(({}))
+    await appendFile('./log.txt', `processed by ${process.pid}\n`)
+
+    const result = Array.from({ length: 1e3 }, _ => Math.floor(Math.random() * 40))
+    .reduce((prev, next) => prev + next, 0)
+
+    response.end(result.toString())
 }
 
 createServer(handler)
     .listen(3000, () => console.log(`Server running at 3000 AND pid ${process.pid}`))
 
-// TODO: 05:15 https://training.erickwendel.com.br/92103-formacao-javascript-expert/2196749-turbinando-uma-web-api-existente-com-o-modulo-cluster-teste-de-carga-com
+// TODO: 07:07 https://training.erickwendel.com.br/92103-formacao-javascript-expert/2196749-turbinando-uma-web-api-existente-com-o-modulo-cluster-teste-de-carga-com
