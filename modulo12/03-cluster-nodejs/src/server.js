2e78e9a3d1fdf5cd5f2b6cced49a02866b8aea74
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-07-06 13:08:04 -0300

    feat: class 5 module 12

--- modulo12/03-cluster-nodejs/src/server.js
+++ modulo12/03-cluster-nodejs/src/server.js
@@ -1,16 +1,19 @@
 import { createServer } from 'http'
-import { appendFile } from 'fs'
+import { appendFile } from 'fs/promises'
 
-async function handler(request, response) {
-    await appendFile('./log.txt', `processed by ${process.pid}\n`)
+export function initializeServer() {
 
-    const result = Array.from({ length: 1e3 }, _ => Math.floor(Math.random() * 40))
-    .reduce((prev, next) => prev + next, 0)
+    async function handler(_, response) {
+        await appendFile('./log.txt', `processed by ${process.pid}\n`) // cat log.txt | grep 934272 | wc -l
 
-    response.end(result.toString())
+        const result = Array.from({ length: 1e3 }, _ => Math.floor(Math.random() * 40))
+        .reduce((prev, next) => prev + next, 0)
+
+        response.end(result.toString())
+    }
+
+    createServer(handler)
+        .listen(3000, () => console.log(`Server running at 3000 AND pid ${process.pid}`))
+
+    setTimeout(() => process.exit(1), Math.random() * 1e4)
 }
-
-createServer(handler)
-    .listen(3000, () => console.log(`Server running at 3000 AND pid ${process.pid}`))
-
-// TODO: 07:07 https://training.erickwendel.com.br/92103-formacao-javascript-expert/2196749-turbinando-uma-web-api-existente-com-o-modulo-cluster-teste-de-carga-com
