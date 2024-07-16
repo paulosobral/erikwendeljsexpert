afecdd0b11122705f8f8c26fe3bedbb539c86cb1
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-07-16 08:08:49 -0300

    feat: class 04 module 13

--- modulo13/01-child-process/01-spawn-process/uploads/server.js
+++
@@ -1,9 +0,0 @@
-import { createServer } from 'http'
-
-async function handler(request, response) {
-
-}
-
-createServer(handler)
-.listen(3000, () => console.log('Server is running at 3000'))
-// TODO 07:43 https://training.erickwendel.com.br/92103-formacao-javascript-expert/2196754-chamando-outras-linguagens-de-programacao-a-partir-do-node-js-com-child-
