e9d8cf8322c927b9c0ea2c7f36559a312d7a4684
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-07-17 08:59:08 -0300

    wip: class 05 module 13

---
+++ modulo13/01-child-process/02-process-report/src/index.js
@@ -0,0 +1,4 @@
+import { fork } from 'child_process'
+
+const file = './data/All_pokemon2.csv'
+// TODO 07:02 https://training.erickwendel.com.br/92103-formacao-javascript-expert/2196755-processamento-massivo-em-arquivos-usando-child-processes-fork
