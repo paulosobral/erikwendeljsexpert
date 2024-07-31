42fe9bc77bfb128b16a63996202ec388512b0553
Paulo Sobral <paulo.sobral@outlook.com.br>
2024-07-31 13:23:00 -0300

    wip: class 05 module 13

--- modulo13/01-child-process/02-process-report/src/backgroundTask.js
+++ modulo13/01-child-process/02-process-report/src/backgroundTask.js
@@ -1,3 +1,3 @@
-process.on('message', (msg) => console.log('msg from child', msg.toString()))
+process.on('message', (msg) => console.log('msg from child', msg.Name))
 
-console.log(`I'm ready!! ${process.pid}`)
+// console.log(`I'm ready!! ${process.pid}`)
--- modulo13/01-child-process/02-process-report/src/index.js
+++ modulo13/01-child-process/02-process-report/src/index.js
@@ -1,12 +1,18 @@
 import { fork } from 'child_process'
-// import { setTimeout } from 'timers/promises'
-const file = './data/All_pokemon2.csv'
+import { setTimeout } from 'timers/promises'
+import { createReadStream } from 'fs'
+import { pipeline } from 'stream/promises'
+import { Writable } from 'stream'
+import csvtojson from 'csvtojson'
+
+const database = './data/All_Pokemon2.csv'
+
 const PROCESS_COUNT = 10;
 
 const backgroundTaskFile = './src/backgroundTask.js'
 const processes = new Map()
 for(let index=0; index < PROCESS_COUNT; index++) {
-    const child = fork(backgroundTaskFile, [file])
+    const child = fork(backgroundTaskFile, [database])
     processes.set(child.pid, child)
 }
 
@@ -20,6 +26,18 @@ function roundRoubin(array, index=0) {
 }
 // Pooll de conexões, ou load balancer
 const getProcess = roundRoubin([...processes.values()])
-for(let index = 0; index < 100; index++)
-    console.count(getProcess().pid)
-// TODO 20:30 https://training.erickwendel.com.br/92103-formacao-javascript-expert/2196755-processamento-massivo-em-arquivos-usando-child-processes-fork
+console.log(`startting with ${processes.size} processes`)
+await setTimeout(100)
+
+await pipeline(
+    createReadStream(database),
+    csvtojson(),
+    Writable({
+        write(chunk, enc, cb) {
+            const chosenProcess = getProcess()
+            chosenProcess.send(JSON.parse(chunk))
+            cb()
+        }
+    })
+)
+// TODO 24:24 https://training.erickwendel.com.br/92103-formacao-javascript-expert/2196755-processamento-massivo-em-arquivos-usando-child-processes-fork
